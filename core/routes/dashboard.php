9a520fb3fbaf2dcba9d6be35c831b7812a3ec565
abdalla mansour <a.mansour.code@gmail.com>
2025-12-02 21:57:15 +0200

    Refactor dashboard route to remove backend path prefix for improved clarity and maintainability

--- core/routes/dashboard.php
+++ core/routes/dashboard.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Dashboard\TagController;
 use App\Http\Controllers\Dashboard\PopupController;
 use Illuminate\Support\Facades\Route;
 
-Route::Group(['prefix' => getBackendPath(), 'middleware' => ['auth', 'LanguageSwitcher', 'is_valid_tenant']], function () {
+Route::Group(['middleware' => ['auth', 'LanguageSwitcher', 'is_valid_tenant']], function () {
 // Admin Home
     Route::get('/', [DashboardController::class, 'index'])->name('adminHome');
 //Search
